7dc7952d3fae565be511d3ad7d581fe156e904b0
AllesBlaw <103613091+NickSigler@users.noreply.github.com>
2023-11-12 17:45:27 -0300

    Update BubbleSort.java

--- estruturasOrdenacao/BubbleSort.java
+++ estruturasOrdenacao/BubbleSort.java
@@ -10,24 +10,44 @@ public class BubbleSort
         this.array = array;
         this.tamanho = tamanho;
     }
-    public void ordenar(int tamanho, int numero)
+    public void ordenar(int tamanho, int numero, int contador)
     {
+        
         int analisando = array[numero];
-        if(analisando > array[numero + 1])
+        if(numero == array.length - 1)
         {
-            array[numero] = array[numero + 1];
-            array[numero + 1] = analisando;
-            analisando++;
-
-            System.out.println("Estado atual do array: " + Arrays.toString(array));
-
-            ordenar(1, analisando);
+            ordenar(tamanho, 0, 0);
         }
         else
         {
+            
+            if(analisando > array[numero + 1])
+            {
+                array[numero] = array[numero + 1];
+                array[numero + 1] = analisando;
+                numero++;
 
+                System.out.println("Estado atual do array: " + Arrays.toString(array));
 
+                ordenar(1, numero, contador);
+            }
+            else if(contador == array.length - 1)
+            {
+                System.out.println("Vetor ordenado!");
+            }
+            else
+            {
+
+                numero++;
+                contador++;
+                ordenar(tamanho, numero, contador);
+                
+
+            }
         }
+        
+        
+        
 
     }
 
