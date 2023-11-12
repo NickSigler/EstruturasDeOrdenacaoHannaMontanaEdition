afa69d33e7f9cf4987869e5cb7ec6d4b9363ffc6
AllesBlaw <103613091+NickSigler@users.noreply.github.com>
2023-11-12 17:11:03 -0300

    Update ResultadoQuickSort.java

--- resultados/ResultadoQuickSort.java
+++ resultados/ResultadoQuickSort.java
@@ -1,5 +1,37 @@
 package resultados;
 
+import estruturasOrdenacao.QuickSort;
+
 public class ResultadoQuickSort
 {
+    public static void main(String[] args)
+    {
+
+        int[] array = new int[20];
+
+        array[0] = 49;
+        array[1] = 15;
+        array[2] = 40;
+        array[3] = 27;
+        array[4] = 20;
+        array[5] = 19;
+        array[6] = 50;
+        array[7] = 2;
+        array[8] = 12;
+        array[9] = 28;
+        array[10] = 49;
+        array[11] = 44;
+        array[12] = 9;
+        array[13] = 59;
+        array[14] = 18;
+        array[15] = 5;
+        array[16] = 30;
+        array[17] = 6;
+        array[18] = 7;
+        array[19] = 28;
+
+
+        QuickSort quickSort = new QuickSort();
+        quickSort.ordenar(array, 0, 0);
+}
 }
